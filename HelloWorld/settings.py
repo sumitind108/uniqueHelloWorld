27d04f5a2939d94cd92d710d5858178b99b5eeb9
Sumit Yadav <syadav1@greencellmobility.com>
2023-10-16 12:03:51 +0530

    host error fixing

--- HelloWorld/settings.py
+++ HelloWorld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+nkbi_mrp##acc1es44tus8z9#m9bx!(f2poev*t_@c57e)*x='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
